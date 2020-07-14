c840654a2d3e482776ad02ad0ef13368cfd2b837
Anshul Arya <54198096+Anshul-Arya@users.noreply.github.com>
2020-07-15 01:00:06 +1000

    Running Proprotion Bayesian Example

---
+++ RunningProportion.R
@@ -0,0 +1,30 @@
+# Goal: Toss a coin N times and compute the running proportion of heads.
+N = 500
+# Generate a random sample of N flips for a fair coin (heads=1, tails=0);
+# the function "sample" is part of R:
+set.seed(47405) # Comment to set the "seed" for the random number generator.
+flipsequence = sample(x = c(0,1), prob = c(.5, .5), size = N, replace = TRUE)
+# Compute the running proportion of heads
+r = cumsum(flipsequence)
+n = 1:N
+runprop = r / n # Component by component division
+# graph the running proportion
+# To Learn about the parameters of the plot function
+plot( n , runprop , type="o" , log="x" ,
+      xlim=c(1,N) , ylim=c(0.0,1.0) , cex.axis=1.5 ,
+      xlab="Flip Number" , ylab="Proportion Heads" , cex.lab=1.5 ,
+      main="Running Proportion of Heads" , cex.main=1.5,
+      col = "darkgreen")
+# Plot a dotted horizontal line at y=.5, just as a reference line:
+lines(c(1,N), c(.5,.5), lty = 3, col = "red")
+# Display the beginning of the flip sequence. These string and character
+# manipulations may seem mysterious, but you can de-mystify by unpacking
+# the commands starting with the innermost parentheses or brackets and
+# moving to the outermost.
+flipletters = paste(c("T","H")[flipsequence[ 1:10] + 1], collapse = "")
+displaystring = paste("Flip Sequence = ", flipletters,  "...", sep = "")
+text(5, .9, displaystring, adj = c(0,1), cex = 1.3)
+# Display the relative frequency at the end of the sequence
+text(N, .3, paste("End Proportion =", runprop[N]), adj = c(1,0), cex = 1.3)
+# Save the plot to an EPS file
+dev.copy2pdf(file = "RunningProportion.pdf")
